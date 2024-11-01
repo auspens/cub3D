b13455653aaa84ee8ab964f11c871757ad196c89
Aleksandra Uspenskaia <auspensk@c3a2c9.42berlin.de>
2024-11-01 12:55:23 +0100

    parcing added

--- map_parcing/check_map.c
+++ map_parcing/check_map.c
@@ -6,13 +6,13 @@
 /*   By: auspensk <auspensk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 10:44:51 by auspensk          #+#    #+#             */
-/*   Updated: 2024/10/29 17:03:34 by auspensk         ###   ########.fr       */
+/*   Updated: 2024/11/01 11:50:02 by auspensk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-int	should_be_one(t_data *data, int x, int y)
+int	should_be_wall(t_data *data, int x, int y)
 {
 	if (x == 0 || data->map[y][x + 1] == '\n' || data->map[y][x + 1] == ' '
 		|| data->map[y][x - 1] == ' ')
@@ -58,6 +58,8 @@ void	set_player(t_data *data, int x, int y)
 	data->player.y = (double)y + 0.5;
 	set_direction(data, data->map[y][x]);
 	rotate_vector(data->dir, &(data->plane), PI / 2);
+	data->plane.x *= 0.66;
+	data->plane.y *= 0.66;
 	data->map[y][x] = '0';
 }
 
@@ -70,11 +72,11 @@ void	check_valid_map(t_data *data)
 	y = -1;
 	while (data->map[++y])
 	{
-		while (data->map[y][++x] != '\n')
+		while (data->map[y][++x] && data->map[y][x] != '\n')
 		{
-			if (should_be_one(data, x, y) && data->map[y][x] != '1')
+			if (should_be_wall(data, x, y) && data->map[y][x] != '1')
 				clean_exit(1, "Err: map has to be surrounded by walls\n", data);
-			if (!should_be_one(data, x, y))
+			if (!should_be_wall(data, x, y))
 			{
 				if (data->map[y][x] == 'N' || data->map[y][x] == 'S'
 					|| data->map[y][x] == 'E' || data->map[y][x] == 'W')
