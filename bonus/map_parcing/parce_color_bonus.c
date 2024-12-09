74fcb0398f96ebee2b5eef6528efd385e8541132
Aleksandra Uspenskaia <auspensk@c3a2c9.42berlin.de>
2024-12-09 14:11:36 +0100

    map parcing bugs fixed

--- bonus/map_parcing/parce_color_bonus.c
+++ bonus/map_parcing/parce_color_bonus.c
@@ -6,60 +6,78 @@
 /*   By: auspensk <auspensk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 13:07:37 by auspensk          #+#    #+#             */
-/*   Updated: 2024/11/29 11:24:35 by auspensk         ###   ########.fr       */
+/*   Updated: 2024/12/09 13:24:50 by auspensk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d_bonus.h"
 
-void	exit_at_reading_color(t_data *data, int fd, char **arr, char **colors)
+void	exit_at_reading_color(t_data *data, int fd, char **arr)
 {
-	if (colors)
-		free_array(colors);
 	free_array(arr);
 	close(fd);
 	clean_exit(1, "Map error in colors\n", data);
 }
 
-int	read_color(t_data *data, char **lines, int fd, char **colors)
+int	read_color(char *line, int *rgb)
 {
-	int	rgb[3];
-	int	ind;
+	int		i;
+	char	color[4];
 
-	ind = -1;
-	while (colors[++ind])
+	i = -1;
+	while (ft_isdigit(line[++i]) && i < 4)
+		color[i] = line[i];
+	if (i < 1 || i > 4)
+		return (-1);
+	color[i] = '\0';
+	*rgb = ft_atoi(color);
+	if (*rgb < 0 || *rgb > 255)
+		return (-1);
+	return (i);
+}
+
+int	check_color(t_data *data, char **lines, int fd)
+{
+	int	i;
+	int	c_i;
+	int	c_move;
+	int	rgb[3];
+
+	i = -1;
+	c_i = 0;
+	while ((lines[1][c_i]))
 	{
-		if (!ft_isnum(colors[ind]))
-			exit_at_reading_color(data, fd, lines, colors);
-		rgb[ind] = ft_atoi(colors[ind]);
-		if (rgb[ind] > 255 || rgb[ind] < 0)
-			exit_at_reading_color(data, fd, lines, colors);
+		i ++;
+		c_move = read_color(&(lines[1][c_i]), &(rgb[i]));
+		if (c_move == -1)
+			exit_at_reading_color(data, fd, lines);
+		c_i += c_move;
+		if (lines[1][c_i] == ',' && i < 2)
+			c_i++;
+		else if (lines[1][c_i] != '\0')
+			exit_at_reading_color(data, fd, lines);
 	}
-	if (ind != 3)
-		exit_at_reading_color(data, fd, lines, colors);
+	if (i != 2)
+		exit_at_reading_color(data, fd, lines);
 	return (gen_trgb(255, rgb[0], rgb[1], rgb[2]));
 }
 
 int	parce_color(t_data *data, char **lines, int fd)
 {
-	char	**colors;
-
 	if (!lines[1] || lines[2])
-		exit_at_reading_color(data, fd, lines, NULL);
-	colors = ft_split(lines[1], ',');
+		exit_at_reading_color(data, fd, lines);
 	if (!ft_strcmp(lines[0], "F"))
 	{
 		if (data->color_floor)
-			exit_at_reading_color(data, fd, lines, colors);
-		data->color_floor = read_color(data, lines, fd, colors);
+			exit_at_reading_color(data, fd, lines);
+		data->color_floor = check_color(data, lines, fd);
 	}
 	if (!ft_strcmp(lines[0], "C"))
 	{
 		if (data->color_ceiling)
-			exit_at_reading_color(data, fd, lines, colors);
-		data->color_ceiling = read_color(data, lines, fd, colors);
+			exit_at_reading_color(data, fd, lines);
+		data->color_ceiling = check_color(data, lines, fd);
 	}
 	free_array(lines);
-	free_array(colors);
 	return (1);
 }
